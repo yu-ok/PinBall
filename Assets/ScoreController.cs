7b7e999ff55592c005f4c69d94fcae6bcdfb8696
yu-ok <71647136+yu-ok@users.noreply.github.com>
2020-10-07 14:50:25 +0900

    Initial commit

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{   
+    private int score = 0;
+    private GameObject scoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text> ().text = score.ToString();
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag" || tag == "SmallCloudTag")
+            {
+                this.score += 10;
+            }
+        else if (other.gameObject.tag == "LargeStarTag" || tag == "LargeCloudTag")
+            {
+                this.score += 20;
+            }
+    }
+}
---
+++ Assets/StarController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour
+{
+    private float rotSpeed = 0.5f;
+
+    void Start ()
+    {
+        this.transform.Rotate (0, Random.Range (0, 360), 0);
+        
+    }
+
+    void Update()
+    {
+        this.transform.Rotate (0, this.rotSpeed, 0);
+    }
+}
